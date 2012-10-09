1dea8f974be30b2a0d89658b599c723b0842745e
Justin Blake <justin@hentzia.com>
2012-10-09 18:19:31 -0400

    Turn the bot into a for real dj.

--- yodj.js
+++ yodj.js
@@ -2,7 +2,7 @@ config = {
   "auth": process.env.TT_AUTH,
   "user_id": process.env.TT_USERID,
   "room_id": process.env.TT_ROOMID
-}
+};
 
 var Bot = require("ttapi");
 var bot = new Bot(config.auth, config.user_id, config.room_id);
@@ -10,10 +10,27 @@ var bot = new Bot(config.auth, config.user_id, config.room_id);
 console.log("Running...");
 
 bot.on("speak", function(data) {
-    console.log("Someone spoke.");
-    if(data.text.match(/bucky/)) {
-        console.log("Someone is talking to me!");
-        bot.speak("Shut up and jam, @" + data.name + "!");
+  console.log("Someone spoke.");
+  if(data.text.match(/^@?bucky/)) {
+    console.log("@" + data.name + " is talking to me.");
+
+    if(data.text.match(/add (this|my) song/)) {
+      bot.speak("I will. I love this song.");
+      bot.bop();
+      bot.roomInfo(false, function(data) {
+        bot.playlistAdd(data.room.metadata.current_song._id);
+      });
     }
+
+    if(data.text.match(/get (you're ass )?up t?here/)) {
+      bot.speak("3 2 1 let's jam");
+      bot.addDj();
+    }
+
+    if(data.text.match(/(get|step) down/)) {
+      bot.speak("As you wish.");
+      bot.removeDj(config.user_id);
+    }
+  }
 });
 
