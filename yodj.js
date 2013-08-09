8da278423a4efda60c7e2e2fdd350afe8c8752c9
Chris Jaynes <chris@yola.com>
2013-08-08 23:19:21 -0500

    Extract named functions

--- yodj.js
+++ yodj.js
@@ -7,56 +7,72 @@ var bot = new Bot(config.auth, config.user_id, config.room_id);
 
 console.log("Running...");
 
-function delay(callback) {
-  setTimeout(callback, config.reply_delay);
+var delay = function(callback, args) {
+  setTimeout(function(){
+    callback(args);
+  }, config.reply_delay);
+};
+
+var addSong = function() {
+  bot.speak("I will. I love this song.");
+  bot.bop();
+  bot.playlistAll(function(playlist) {
+    var song = data.room.metadata.current_song;
+    bot.playlistAdd(song._id, playlist.list.length);
+    console.log("Added " + song.metadata.song + "to my queue");
+  });
+  // show the heart:
+  bot.snag();
+};
+
+var startSpinning = function(){
+  bot.speak("3 2 1 let's jam");
+  bot.addDj();
+  console.log("Started DJ'ing");
 }
 
-var addressedDirectly = function(data){
+var stopSpinning = function(){
+  bot.speak("As you wish.");
+  bot.remDj(config.user_id);
+  console.log("Stopped DJ'ing");
+};
+
+var bop = function() {
+  bot.speak("I was just about to!");
+  bot.bop();
+  console.log("Awesome song is awesome.");
+};
+
+var totallyConfused = function() {
+  bot.speak("Mhmm... Mhmm... I know some of those words.");
+}
+
+var onSpokenTo = function(data){
   console.log("@" + data.name + " is talking to me.");
 
   if(data.text.match(/add (this|my) song/i)) {
-    delay(function() {
-      bot.speak("I will. I love this song.");
-      bot.bop();
-      bot.playlistAll(function(playlist) {
-        var song = data.room.metadata.current_song;
-        bot.playlistAdd(song._id, playlist.list.length);
-        console.log("Added " + song.metadata.song + "to my queue");
-      });
-      // show the heart:
-      bot.snag();
-    });
+    delay(addSong, data);
   }
 
   else if(data.text.match(/get (you're ass )?up t?here/i)) {
-    delay(function() {
-      bot.speak("3 2 1 let's jam");
-      bot.addDj();
-      console.log("Started DJ'ing");
-    });
+    delay(startSpinning, data);
   }
 
   else if(data.text.match(/(get|step) down/i)) {
-    delay(function() {
-      bot.speak("As you wish.");
-      bot.remDj(config.user_id);
-      console.log("Stopped DJ'ing");
-    });
+    delay(stopSpinning, data);
   }
 
   else if(data.text.match(/awesome (this|my) song/i)) {
-    reply(function() {
-      bot.speak("I was just about to!");
-      bot.bop();
-      console.log("Awesome song is awesome.");
-    });
+    delay(bop, data);
   }
 
   else {
-    delay(function() {
-      bot.speak("Mhmm... Mhmm... I know some of those words.");
-    });
+    delay(totallyConfused, data);
   }
+};
+
+var onMentioned = function() {
+  bot.speak("You talking to me...?");
 };
 
 var onSpeak = function(data) {
@@ -68,9 +84,7 @@ var onSpeak = function(data) {
 
   // bucky appears in line but not at the beginning
   else if(data.text.match(/bucky/)) {
-    delay(function() {
-      bot.speak("You talking to me...?");
-    });
+    delay(onMentioned);
   }
 };
 
